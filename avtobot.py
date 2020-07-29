beee9e2cc3ac38a0489e805211bbb97efb56c169
andrew <pixtergoood@gmail.com>
2020-07-30 00:23:50 +0500

    исправил баг со слипом

--- avtobot.py
+++ avtobot.py
@@ -36,7 +36,7 @@ class GoPaste(Login):
                     time.sleep(random.randint(44, 122))
 
             flagForSleep += 1
-            if (flagForSleep == 2):
+            if (flagForSleep < 2):
                 time.sleep(random.randint(3*60*60, 4*60*60))
             else:
                 time.sleep(random.randint(8*60*60, 9*60*60))
